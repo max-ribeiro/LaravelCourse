5b9879cf9042fa4fcaa315313c4720bcc423e6e8
max-ribeiro <max_ribeiro@live.com>
2023-01-30 22:57:54 -0300

    sending data to view

--- app_super_gestao/app/Http/Controllers/TestController.php
+++ app_super_gestao/app/Http/Controllers/TestController.php
@@ -7,6 +7,17 @@ use Illuminate\Http\Request;
 class TestController extends Controller
 {
     public function test(int $p1, int $p2) {
-        echo "{$p1} + {$p2} = ".($p1+$p2);
+        // Send an associative array
+        // return view('site.test', [
+        //     "p1" => $p1,
+        //     "p2" => $p2
+        // ]);
+
+        //Creates an array based on var names ans literal indexes
+        return view('site.test', compact('p1', 'p2'));
+
+        //using the view method
+        // return view('site.test')->with('p1', $p1)->with('p2', $p2);
+
     }
 }
