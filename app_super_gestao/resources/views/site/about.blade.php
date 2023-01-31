9e80f150f1682793fc244f4525235e205fe4b7e2
max-ribeiro <max_ribeiro@live.com>
2023-01-30 21:55:18 -0300

    added basic navigation menu

--- app_super_gestao/resources/views/site/about.blade.php
+++ app_super_gestao/resources/views/site/about.blade.php
@@ -1,1 +1,19 @@
 <h3>Sobre!</h3>
+
+<ul>
+    <li >
+        <a href="/">
+            Home
+        </a>
+    </li>
+    <li>
+        <a href="/contact">
+            Contato
+        </a>
+    </li>
+    <li>
+        <a href="/about">
+            Sobre
+        </a>
+    </li>
+</ul>
--- app_super_gestao/resources/views/site/contact.blade.php
+++ app_super_gestao/resources/views/site/contact.blade.php
@@ -1,1 +1,19 @@
 <h3>Contato!</h3>
+
+<ul>
+    <li >
+        <a href="/">
+            Home
+        </a>
+    </li>
+    <li>
+        <a href="/contact">
+            Contato
+        </a>
+    </li>
+    <li>
+        <a href="/about">
+            Sobre
+        </a>
+    </li>
+</ul>
