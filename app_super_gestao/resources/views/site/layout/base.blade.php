94405f7fae624b8db59ca6b87f5e407ed20dea43
max-ribeiro <max_ribeiro@live.com>
2023-01-31 08:06:58 -0300

    added the base template for extension

---
+++ app_super_gestao/resources/views/site/layout/base.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Super Gestão - Sobre Nós</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="{{ asset('css/default.css') }}" />
+    </head>
+    <body>
+        @yield('body_content')
+        <div class="rodape">
+            <div class="redes-sociais">
+                <h2>Redes sociais</h2>
+                <img src="{{ asset('img/facebook.png') }}">
+                <img src="{{ asset('img/linkedin.png') }}">
+                <img src="{{ asset('img/youtube.png') }}">
+            </div>
+            <div class="area-contato">
+                <h2>Contato</h2>
+                <span>(11) 3333-4444</span>
+                <br>
+                <span>supergestao@dominio.com.br</span>
+            </div>
+            <div class="localizacao">
+                <h2>Localização</h2>
+                <img src="{{ asset('img/mapa.png') }}">
+            </div>
+        </div>
+    </body>
+</html>
