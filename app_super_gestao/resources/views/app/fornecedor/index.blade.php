1f89f4b3d4f9f9e752e5902050fa7c9c160ecc8c
max-ribeiro <max_ribeiro@live.com>
2023-01-31 00:40:21 -0300

    using while on blade

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -3,38 +3,19 @@
 @endphp
 
 @isset($fornecedores)
-  Fornecedor: {{ $fornecedores[0]['nome']}}
-  <br/>
-  Status: {{ $fornecedores[0]['status'] }}
-  <br/>
-  CNPJ: {{ $fornecedores[0]['cnpj'] ?? 'N/A'}}
-  <br/>
-  Telefone: ({{ $fornecedores[0]['ddd'] ?? ''}}) {{ $fornecedores[0]['telefone'] ?? '' }}
-  @switch($fornecedores[0]['ddd'])
-    @case('11')
-      Rio Janeiro - RJ
-      @break
-    @case('12')
-      São Paulo - SP
-      @break
-  @endswitch
+
+  @php $i = 0 @endphp
+  @while(isset($fornecedores[$i]))
+    Fornecedor: {{ $fornecedores[$i]['nome']}}
+    <br/>
+    Status: {{ $fornecedores[$i]['status'] }}
+    <br/>
+    CNPJ: {{ $fornecedores[$i]['cnpj'] ?? 'N/A'}}
+    <br/>
+    Telefone: ({{ $fornecedores[$i]['ddd'] ?? ''}}) {{ $fornecedores[$i]['telefone'] ?? '' }}
+    @php $i++ @endphp
+    <br/>
+    <br/>
+  @endwhile
 @endisset
-
-<br/>
-@for($i = 0; $i < count($fornecedores); $i++)
-  {{$fornecedores[$i]['nome']}}
-  <br/>
-@endfor
-<br/>
-
-@for($i = 0; isset($fornecedores[$i]); $i++)
-  {{$fornecedores[$i]['nome']}}
-  <br/>
-@endfor
-<br/>
-
-@foreach($fornecedores as $fornecedor)
-  {{$fornecedor['nome']}}
-  <br/>
-@endforeach
 <br/>
