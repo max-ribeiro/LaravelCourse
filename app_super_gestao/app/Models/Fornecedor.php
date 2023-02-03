e1a8b127d9f0104c4d0bf1a124ca856cc6e1f6e4
max-ribeiro <max_ribeiro@live.com>
2023-02-03 07:50:07 -0300

    added trait softDelete

--- app_super_gestao/app/Models/Fornecedor.php
+++ app_super_gestao/app/Models/Fornecedor.php
@@ -4,14 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
+    /**
+     * Traits, forma de herança horizontal para contornar a 
+     * não possibilidade de multiplas heranças(extends) da horientação a objetos
+     */
+    use HasFactory;
+    use SoftDeletes;
+
     protected $table = 'fornecedores';
 
     /**
      * Fillable defines wich attributes of class can be edited via Fornecedor::create();
      */
     protected $fillable = ['nome', 'site', 'uf', 'email'];
-    use HasFactory;
+    
 }
