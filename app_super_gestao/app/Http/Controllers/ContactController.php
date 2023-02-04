ba103e5dd8f1e7ef53fb42a6fcd0ab8f9c875f68
max-ribeiro <max_ribeiro@live.com>
2023-02-03 21:54:05 -0300

    basic implementation of addition of new register using the model instance on controler(insertion with form data)

--- app_super_gestao/app/Http/Controllers/ContactController.php
+++ app_super_gestao/app/Http/Controllers/ContactController.php
@@ -3,13 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SiteContato;
+use Exception;
 
 class ContactController extends Controller
 {
     public function show() {
-        var_dump($_POST);
         return view('site.contact', [
             'title' => 'Entre em contato'
         ]);
+    }
+    public function handlePost(Request $request){
+
+        $siteContato = new SiteContato();
+        $siteContato->nome = $request->input('nome');
+        $siteContato->telefone = $request->input('telefone');
+        $siteContato->email = $request->input('email');
+        $siteContato->motivo_contato = $request->input('motivo_contato');
+        $siteContato->mensagem = $request->input('mensagem');
+
+        try {
+            $siteContato->save();
+            return "gravado com sucesso!";
+        } catch(Exception $e) {
+            return $e;
+        }
     }
 }
--- app_super_gestao/resources/views/site/layout/components/form_contact.blade.php
+++ app_super_gestao/resources/views/site/layout/components/form_contact.blade.php
@@ -3,20 +3,20 @@
     @csrf
     @php $class = $class ?? 'borda-preta'; @endphp
 
-    <input type="text" placeholder="Nome" class="{{$class}}" name="name">
+    <input type="text" placeholder="Nome" class="{{$class}}" name="nome">
     <br>
-    <input type="text" placeholder="Telefone" class="{{$class}}" name="tel">
+    <input type="text" placeholder="Telefone" class="{{$class}}" name="telefone">
     <br>
     <input type="text" placeholder="E-mail" class="{{$class}}" name="email">
     <br>
-    <select class="{{$class}}" name="subject">
+    <select class="{{$class}}" name="motivo_contato">
         <option value="1">Qual o motivo do contato?</option>
         <option value="2">Dúvida</option>
         <option value="3">Elogio</option>
         <option value="4">Reclamação</option>
     </select>
     <br>
-    <textarea class="{{$class}}" name="message">Preencha aqui a sua mensagem</textarea>
+    <textarea class="{{$class}}" name="mensagem">Preencha aqui a sua mensagem</textarea>
     <br>
     <button type="submit" class="{{$class}}">ENVIAR</button>
 </form>
