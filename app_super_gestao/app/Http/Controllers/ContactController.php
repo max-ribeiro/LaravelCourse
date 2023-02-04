6dcbf0e10f3046f9b0dca060be0a68e8d17af459
max-ribeiro <max_ribeiro@live.com>
2023-02-03 21:58:33 -0300

    adding data in more simple way using the request->all method

--- app_super_gestao/app/Http/Controllers/ContactController.php
+++ app_super_gestao/app/Http/Controllers/ContactController.php
@@ -15,18 +15,30 @@ class ContactController extends Controller
     }
     public function handlePost(Request $request){
 
-        $siteContato = new SiteContato();
-        $siteContato->nome = $request->input('nome');
-        $siteContato->telefone = $request->input('telefone');
-        $siteContato->email = $request->input('email');
-        $siteContato->motivo_contato = $request->input('motivo_contato');
-        $siteContato->mensagem = $request->input('mensagem');
+        // $siteContato = new SiteContato();
+        // $siteContato->nome = $request->input('nome');
+        // $siteContato->telefone = $request->input('telefone');
+        // $siteContato->email = $request->input('email');
+        // $siteContato->motivo_contato = $request->input('motivo_contato');
+        // $siteContato->mensagem = $request->input('mensagem');
 
-        try {
-            $siteContato->save();
-            return "gravado com sucesso!";
-        } catch(Exception $e) {
-            return $e;
-        }
+        // try {
+        //     $siteContato->save();
+        //     return "gravado com sucesso!";
+        // } catch(Exception $e) {
+        //     return $e;
+        // }
+
+        $siteContato = new SiteContato();
+        // $siteContato->fill($request->all());
+
+        // try {
+        //     $siteContato->save();
+        //     return "gravado com sucesso!";
+        // } catch(Exception $e) {
+        //     return $e;
+        // }
+
+        $siteContato->create($request->all());
     }
 }
